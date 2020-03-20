d9ae7fc799b5a1065dcc0d870c368d8cc590db32
Renny Montero <rennym19@gmail.com>
2020-03-20 15:55:56 -0400

    Updated URL Model

--- shortnd/shorturls/models.py
+++ shortnd/shorturls/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.sites.models import Site
 
 
 class VisitsManager(models.Manager):
     def get_queryset(self):
-        pass
+        return super().get_queryset().order_by('-visit_count')[:100]
 
 
 class URL(models.Model):
-    pass
+    original_url = models.TextField(max_length=2048, unique=True)
+    key = models.CharField(max_length=7, null=True)
+    title = models.CharField(max_length=255, null=True)
+    visit_count = models.IntegerField(null=True, default=0)
+
+    objects = models.Manager()
+    top_hundred = VisitsManager()
+
+    @property
+    def short_url(self):
+        domain = Site.objects.get_current().domain
+        return 'http://{0}/{1}/'.format(domain, self.key)
