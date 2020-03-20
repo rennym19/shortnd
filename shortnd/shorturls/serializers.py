29196e683f43857f438952e2c39ba06febfdd1c6
Renny Montero <rennym19@gmail.com>
2020-03-20 15:57:02 -0400

    Adding serializer functionality

--- shortnd/shorturls/serializers.py
+++ shortnd/shorturls/serializers.py
@@ -1,5 +1,19 @@
 from rest_framework import serializers
-from shorturls.models import URL
+from ..models import URL
+from ..key_generator import gen_key
 
 class URLSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = URL
+        fields = ['id', 'original_url', 'key', 'short_url', 'title', 'visit_count']
+        read_only_fields = ['id', 'visit_count', 'short_url', 'key']
+
+    def validate_original_url(self, val):
+        return val
+
+    def create(self, validated_data):
+        url = URL(**validated_data)
+        url.save()
+
+        url.key = gen_key(url.pk)
+        return url
